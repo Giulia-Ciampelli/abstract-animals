a57fae2d85d62120e4fb0dae01ad73d751e30111
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-03-03 10:07:58 +0100

    docs: methods test

--- abstract-animals/src/App.java
+++ abstract-animals/src/App.java
@@ -9,5 +9,18 @@ public class App {
         Cane cane1 = new Cane();
         Aquila aquila1 = new Aquila();
         Delfino delfino1 = new Delfino();
+
+        passerotto1.dormi();
+        passerotto1.mangia();
+        passerotto1.verso();
+        cane1.dormi();
+        cane1.mangia();
+        cane1.verso();
+        aquila1.dormi();
+        aquila1.mangia();
+        aquila1.verso();
+        delfino1.dormi();
+        delfino1.mangia();
+        delfino1.verso();
     }
 }
